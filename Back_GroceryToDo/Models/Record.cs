e37e755e275aec1b19eb8aaddd17d133caab4880
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-03-16 19:57:59 +0100

    descr added

--- Back_GroceryToDo/Models/Record.cs
+++ Back_GroceryToDo/Models/Record.cs
@@ -1,4 +1,4 @@
-﻿﻿using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Back_GroceryToDo.Models
@@ -6,6 +6,7 @@ namespace Back_GroceryToDo.Models
     public class Record
     {
         [JsonPropertyName("id")] public int Id { get; set; }
+        [JsonPropertyName("description")] public string Description { get; set; }
         [JsonPropertyName("items")] public List<Item> Items { get; set; }
     }
 }
